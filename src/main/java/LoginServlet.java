64d9e47263a5268fb11932f411b28bbf92a5bd7d
samruddhi randive <samruddhirandive97@gmail.com>
2022-01-27 09:37:59 +0530

    Changes have been committed

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,54 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+           
+        String userId = request.getParameter("userid");
+        //creating a new hidden form field
+       out.println("<form action='dashboard' method='post'>");
+       out.println("<input type='hidden' name='userid' id='userid' value='"+userId+"'>");
+       out.println("<input type='submit' value='submit' >");
+       out.println("</form>");
+       out.println("<script>document.forms[0].submit();</script>");
+     
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
